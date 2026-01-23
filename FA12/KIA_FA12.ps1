9c55dcc360bf46746c40c7a6abbc741815cc595c
Hồng Bảo Ngân <hongbaongan@gmail.com>
2026-01-23 08:56:37 +0700

    Update KIA_FA12.ps1
    Update path

--- FA12/KIA_FA12.ps1
+++ FA12/KIA_FA12.ps1
@@ -20,7 +20,7 @@ Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
 # Bước 3: Copy shortcut ra thư mục Public Desktop
 # Giả sử tên shortcut là "FAST.lnk" (thay thế bằng tên thực tế nếu khác)
 $shortcutName = "KIA_FA12.lnk"  # Thay thế bằng tên shortcut thực tế trong file nén
-$shortcutPath = Join-Path -Path $extractPath -ChildPath "KIA_FA12\$shortcutName"  # Giả sử shortcut nằm trong thư mục KIA_FA12
+$shortcutPath = Join-Path -Path $extractPath -ChildPath "\$shortcutName"  # Giả sử shortcut nằm trong thư mục
 $publicDesktop = "C:\Users\Public\Desktop"
 
 if (Test-Path $shortcutPath) {
