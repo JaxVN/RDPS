12261892462f4539550637990f8087db28abc8d5
Hồng Bảo Ngân <hongbaongan@gmail.com>
2026-01-26 11:04:30 +0700

    Update KSL_FA12.ps1

--- FA12/KSL_FA12.ps1
+++ FA12/KSL_FA12.ps1
@@ -3,7 +3,7 @@
 $url = "https://kaglav.sharepoint.com/:u:/s/IT/IQBcE_3oBN__TIjEg_BA_59bAZXuQ3-TftE0szo17Sl_nN8?e=cPqpFS&download=1"  # Thay thế bằng đường dẫn thực tế đến file KIA_FA12.Zip +"&download=1" 
 
 # Đường dẫn lưu file zip và thư mục đích
-$zipPath = "C:\FAST\KIA_FA12.Zip"
+$zipPath = "C:\FAST\KSL_FA12.Zip"
 $extractPath = "C:\FAST"
 
 # Tạo thư mục C:\FAST nếu chưa tồn tại
@@ -14,12 +14,12 @@ if (-not (Test-Path $extractPath)) {
 # Bước 1: Tải file nén từ SharePoint về C:\FAST
 Invoke-WebRequest -Uri $url -OutFile $zipPath
 
-# Bước 2: Giải nén file KIA_FA12.Zip vào C:\FAST
+# Bước 2: Giải nén file KSL_FA12.Zip vào C:\FAST
 Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
 
 # Bước 3: Copy shortcut ra thư mục Public Desktop
 # Giả sử tên shortcut là "FAST.lnk" (thay thế bằng tên thực tế nếu khác)
-$shortcutName = "KIA_FA12.lnk"  # Thay thế bằng tên shortcut thực tế trong file nén
+$shortcutName = "KSL_FA12.lnk"  # Thay thế bằng tên shortcut thực tế trong file nén
 $shortcutPath = Join-Path -Path $extractPath -ChildPath $shortcutName  # Shortcut nằm trực tiếp trong C:\FAST sau giải nén
 $publicDesktop = "C:\Users\Public\Desktop"
 
