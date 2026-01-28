c1d04ee76f56aed9c8967bc0aec86b0e07448c21
Hồng Bảo Ngân <hongbaongan@gmail.com>
2026-01-28 16:19:06 +0700

    Update STL_FA12.ps1

--- FA12/STL_FA12.ps1
+++ FA12/STL_FA12.ps1
@@ -3,7 +3,7 @@
 $url = "https://kaglav.sharepoint.com/:u:/s/IT/IQBGQYifSKSXQZr4ARZ51dPgAee4RgIHRCs_5lxuiPKldBU?e=cT2IPT&download=1"  # Thay thế bằng đường dẫn thực tế đến file KIA_FA12.Zip +"&download=1" 
 
 # Đường dẫn lưu file zip và thư mục đích
-$zipPath = "C:\FAST\KIA_FA12.Zip"
+$zipPath = "C:\FAST\STL_FA12.Zip"
 $extractPath = "C:\FAST"
 
 # Tạo thư mục C:\FAST nếu chưa tồn tại
@@ -19,7 +19,7 @@ Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force
 
 # Bước 3: Copy shortcut ra thư mục Public Desktop
 # Giả sử tên shortcut là "FAST.lnk" (thay thế bằng tên thực tế nếu khác)
-$shortcutName = "KIA_FA12.lnk"  # Thay thế bằng tên shortcut thực tế trong file nén
+$shortcutName = "STL_FA12.lnk"  # Thay thế bằng tên shortcut thực tế trong file nén
 $shortcutPath = Join-Path -Path $extractPath -ChildPath $shortcutName  # Shortcut nằm trực tiếp trong C:\FAST sau giải nén
 $publicDesktop = "C:\Users\Public\Desktop"
 
